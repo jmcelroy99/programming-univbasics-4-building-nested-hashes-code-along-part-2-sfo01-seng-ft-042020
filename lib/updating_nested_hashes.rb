23cc9ec568a51e3eadb5fa54d56750d5a84b4995
energetic-commit-5967 <jmcelroy@plexustech.net>
2020-04-17 19:51:18 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -60,7 +60,10 @@ def add_characters
    
    
    def add_characters
-     
+     epic_tragedy[:additional_characters] = [
+  { name: "Prince Escalus" },
+  { name: "Apothecary" }
+]
    
    epic_tragedy
 end 
